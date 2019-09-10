6f4fbccadea5e9b73ebe34e3b206162d3ec055c0
Kristjan Luur <kluur@users.noreply.github.com>
2019-09-10 11:09:16 +0300

    esimene üleslaadimine

---
+++ 02tund/myindex.php
@@ -0,0 +1,22 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Veebiprogrammeerimise leht</title>
+
+</head>
+<body>
+  <h1>Kristjan Luur, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiselt võetavat sisu!</p>
+  <a href="https://www.google.com">See on link Googlisse</a>
+  <hr>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</p>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+  
+</body>
+</html>
---
+++ 02tund/testpage.php
@@ -0,0 +1,24 @@
+<?php
+  $userName = "Kristjan Luur";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+  programmeerib veebi</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName . ", veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiselt võetavat sisu!</p>
+  
+  <a href="https://www.google.com">See on link Googlisse</a>
+  
+</body>
+</html>
