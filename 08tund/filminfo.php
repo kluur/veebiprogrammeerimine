50831d9998d681a797117ee7f6be75f3f52372da
Kristjan Luur <kluur@users.noreply.github.com>
2019-10-29 11:30:28 +0200

    fotode üleslaadimine lisatud

---
+++ 08tund/filminfo.php
@@ -0,0 +1,28 @@
+<?php
+  require("../../../config_vp2019.php");
+  require("functions_film.php");
+  $userName = "Kristjan Luur";
+  $database = "if19_kristjan_lu_1";
+  
+  $filmInfoHTML = readAllFilms();
+	
+  //lisame lehe päise
+  require("header.php");
+?>
+
+
+<body>
+  <?php
+    echo "<h1>" .$userName ." koolitöö leht</h1>";
+  ?>
+  <p>See leht on loodud koolis õppetöö raames
+  ja ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <h2>Eesti filmid</h2>
+  <p>Praegu on andmebaasis järgmised filmid:</p>
+  <?php
+	//echo "Server: " .$serverHost .", kasutaja: " .$serverUsername;
+	echo $filmInfoHTML;
+  ?>
+</body>
+</html>
